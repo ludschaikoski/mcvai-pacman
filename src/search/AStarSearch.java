6806119d4220f083446ad70e478e53b5d29b57dd
a.mounir86 <a.mounir86@c185dd7a-e5c7-11de-b136-e12340cf38ae>
2009-12-16 14:55:09 +0000

    Modified the code, corrected some mistakes.

--- src/search/AStarSearch.java
+++ src/search/AStarSearch.java
@@ -7,22 +7,63 @@ import java.util.List;
 
 import pacman.Game;
 import pacman.Location;
-import pacman.LocationSet;
 import pacman.Move;
 import pacman.State;
 import util.PriorityQueue;
 
+/**
+ * <p>
+ *  In this class we implement the A* search technique for a Pacman game.
+ *  </p>
+ *
+ * <p>
+ *  The A* search need a heuristic and it calculates the function (f) from
+ * a certain node as the cost from the starting node to that node + the heuristic
+ * function from that node to the goal
+ * </p>
+ *
+ * @author amounir, eartola
+ *
+ */
 public class AStarSearch implements Search {
 	
+	/**
+	 * The heuristic we are going to use
+	 */
 	Heuristic aStarHeuristic;
 	
+	/**
+	 * The constructor should read the appropriate heuristic that should be used.
+	 *
+	 * @param aStarHeuristic the AStar heuristic used in the construction.
+	 */
 	public AStarSearch(Heuristic aStarHeuristic) {
 		this.aStarHeuristic = aStarHeuristic;
 	}
 
+	/**
+	 *  Overrides the search for location state using the A* technique.
+	 *
+	 *  - The function (f) =
+	 *      cost to the current node (g) + heuristic cost to goal node (h)
+	 *
+	 *  We use some hash tables that do the following:
+	 *    - Get the move that brought me to this location
+	 *    - Get the cost till I came to this location
+	 *    - Get the state I am in on that location
+	 *
+	 *  To get the next node to be traversed we used a priority queue where priority
+	 * is the function (f)
+	 *    
+	 *  In the end of the function we return the list of moves that brought us to
+	 * that location
+	 */
 	@Override
-	public List<Move> searchForState(State starting, Location goal) {
+	public List<Move> searchForLocation(State starting, Location goal) {
+		// Initialize our priority queue
 		PriorityQueue<Location> aStarPQ = new PriorityQueue<Location>();
+		
+		// Initialize the hashmaps that we are using
 		HashMap<Location, Move> moveToLocation = new HashMap<Location, Move>();
 		HashMap<Location, Integer> costToLocation = new HashMap<Location, Integer>();
 		HashMap<Location, State> locationState = new HashMap<Location, State>();
@@ -30,7 +71,7 @@ public class AStarSearch implements Search {
 		aStarPQ.add(starting.getPacManLocation(), aStarHeuristic.calculateHeuristicCost(starting.getPacManLocation(), goal));
 		moveToLocation.put(starting.getPacManLocation(), Move.NONE); // We made no moves.
 		costToLocation.put(starting.getPacManLocation(), 0); // We paid nothing to go to the same place.
-		locationState.put(starting.getPacManLocation(), starting);
+		locationState.put(starting.getPacManLocation(), starting); // The state is the starting state.
 		while (!aStarPQ.isEmpty()) {
 			Location current = aStarPQ.removeFirst();
 
@@ -60,15 +101,12 @@ public class AStarSearch implements Search {
 					moveToLocation.put(nextState.getPacManLocation(), move);
 					locationState.put(nextState.getPacManLocation(), nextState);
 					costToLocation.put(nextState.getPacManLocation(), costToLocation.get(current) + 1);
-					aStarPQ.add(nextState.getPacManLocation(), costToLocation.get(nextState.getPacManLocation()) +
-							aStarHeuristic.calculateHeuristicCost(nextState.getPacManLocation(), goal));
+					aStarPQ.add(nextState.getPacManLocation(), -1.0 * (costToLocation.get(nextState.getPacManLocation()) +
+							aStarHeuristic.calculateHeuristicCost(nextState.getPacManLocation(), goal)));
 				}
 			}
 		}
 		
-		return null;
+		return null; // Return null if we cannot come to that location.
 	}
-	
-	
-	
 }
